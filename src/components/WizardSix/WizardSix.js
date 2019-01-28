8c5ff46bd261ee0301568bc96a575e6518ac33d1
Caleb Smith <calebsupport@Calebs-MacBook-Air.local>
2019-01-28 17:09:48 -0600

    Finished afternoon project. Functional

--- src/components/WizardSix/WizardSix.js
+++ src/components/WizardSix/WizardSix.js
@@ -1,28 +1,45 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { updateDownPayment, updateCost } from '../../ducks/reducer';
 
 class WizardSix extends Component {
+	render() {
+		const { updateDownPayment, updateCost } = this.props;
+		return (
+			<div className="parent-div">
+				<div className="vert-align">
+					{' '}<p>What is the estimated purchase price?</p> <br />
+					<input
+						type="text"
+						placeholder="amount"
+						onChange={e => updateCost(e.target.value)}
+					/>{' '}
+					<br />
+					<p>How much are you putting down as a down payment?</p>{' '}
+					<br />
+					<input
+						type="text"
+						placeholder="amount"
+						onChange={e => updateDownPayment(e.target.value)}
+					/>
+					<Link to="/wSeven">
+						<button className="margin-btn"> Next </button>
+					</Link>
+				</div>
+			</div>
+		);
+	}
+}
+function mapStateToProps(state) {
+	const { payment, cost } = state;
 
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">                    <p>What is the estimated purchase price?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateCost}/> <br />
-
-
-                    <p>How much are you putting down as a down payment?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateDownPayment}/>                    
-                        
-                    
-                    <Link to="/wSeven"><button className="margin-btn"> Next </button></Link>
-                </div>
-            </div>
-        )
-    }
+	return {
+		payment,
+		cost
+	};
 }
 
-export default WizardSix;
+export default connect(mapStateToProps, { updateDownPayment, updateCost })(
+	WizardSix
+);
